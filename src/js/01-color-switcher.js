8da153b56b3de4710c135812db7db1d7dfde9d2c
NEXT1990 <maksim.listunov@gmail.com>
2022-12-08 02:25:10 +0200

    add-task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
+
+
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', event => {
+  colorChenger.starColor();
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+});
+refs.btnStop.addEventListener('click', event => {
+  colorChenger.stopColor();
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+});
+
+const colorChenger = {
+  starColor() {
+    this.intervalId = setInterval(() => {
+      refs.bodyColor.style.backgroundColor = getRandomHexColor();
+      console.log(refs.bodyColor.style.backgroundColor);
+    }, 1000);
+  },
+  stopColor() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  },
+};
