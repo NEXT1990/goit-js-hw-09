e14b55dd2b78808c3ec569090b942b7b84376604
NEXT1990 <maksim.listunov@gmail.com>
2022-12-11 17:00:04 +0200

    add-befor-light-off

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,33 +1,66 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const btnStart = document.querySelector('button[data-start]');
-btnStart.disabled = true;
-console.log(btnStart);
-
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+};
+refs.btnStart.disabled = true;
+console.log(refs.btnStart);
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  intervalId: null,
+
+  onChange() {},
 
   onClose(selectedDates) {
     if (selectedDates[0].getTime() < Date.now()) {
       alert('Please choose a date in the future');
       return;
     }
-    btnStart.disabled = false;
-    btnStart.addEventListener('click', event => {
-      btnStart.disabled = true;
-      setInterval(() => {
+    refs.btnStart.disabled = false;
+    refs.btnStart.addEventListener('click', event => {
+      refs.btnStart.disabled = true;
+      this.intervalId = setInterval(() => {
         const currentTime = Date.now();
         const decrement = selectedDates[0].getTime() - currentTime;
-        console.log(decrement);
+        const { days, hours, minutes, seconds } = convertMs(decrement);
+        console.log(`${days}:${hours}:${minutes}:${seconds}`);
       }, 1000);
+      return this.intervalId;
     });
   },
 };
 
 flatpickr('#datetime-picker', options);
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
 
 console.log(Date.now());
