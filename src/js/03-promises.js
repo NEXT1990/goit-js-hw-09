167e389521003276fd3f969657f488c9770f43aa
NEXT1990 <maksim.listunov@gmail.com>
2022-12-12 01:43:16 +0200

    add-task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('ку-ку, все получилось');
+      } else {
+        reject('О нет, это реджект!!');
+      }
+    }, 3000);
+  });
 }
+
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+console.log(refs.delay, refs.step, refs.amount, refs.btnSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const delay = formElements.delay.value;
+  const step = formElements.step.value;
+  const amount = formElements.amount.value;
+  console.log(formElements);
+  const dataForm = {
+    delay,
+    step,
+    amount,
+  };
+ 
