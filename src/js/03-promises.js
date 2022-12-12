747cfcdf0c42d65c40a04006c9213c5241b28e8d
NEXT1990 <maksim.listunov@gmail.com>
2022-12-12 20:33:57 +0200

    add-task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,24 @@
+import Notiflix from 'notiflix';
+
+/*
+ * @param1 {string}: Required, a text in string format.
+ * @param2 {function | Object}: Optional, a callback function that will be called when the notification element has been clicked. Or, extending the initialize options with the new options for each notification element.
+ * @param3 {Object}: Optional, extending the initialize options with new the options for each notification element. (If the second parameter has been already used for a callback function.)
+ */
+
+let startDelay = 0;
+let stepDelay = 0;
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve('ку-ку, все получилось');
+        resolve();
       } else {
-        reject('О нет, это реджект!!');
+        reject();
       }
-    }, 3000);
+    }, delay);
   });
 }
 
@@ -34,4 +45,22 @@ function onSubmit(event) {
     step,
     amount,
   };
- 
+  let startDelayToNumber = Number(dataForm.delay);
+  let stepDeletToNubmer = Number(dataForm.step);
+
+  for (let position = 0; position < dataForm.amount; position += 1) {
+    let delay = (startDelayToNumber += stepDeletToNubmer);
+
+    createPromise(position, delay)
+      .then((position, delay) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch((position, delay) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
